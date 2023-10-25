4f66c99eb78dbc2d48a7b05ee942a218dd544978
alaa <alaa4souqi@gmail.com>
2023-10-25 15:39:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pushnotificationdemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.pushnotificationdemo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestNotificationPermission()
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    22
+                )
+            }
+        }
+    }
+
+}
